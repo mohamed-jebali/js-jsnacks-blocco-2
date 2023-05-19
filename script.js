aa25d6f8fe52441bcf4fa8120cb1c60b33aa7ae9
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-19 16:37:08 +0200

    working on 1 snack

---
+++ script.js
@@ -0,0 +1,11 @@
+let listNumber = [];
+let inputNumber = 0;
+
+while(inputNumber < 50){
+    parseInt (prompt("Inserisci un Numero"));
+    console.log(inputNumber.push);
+
+    if(inputNumber > 50){
+        console.log()
+    }
+}
