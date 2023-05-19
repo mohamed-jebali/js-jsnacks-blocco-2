9049941427b141ce30889b13b928de9f2f949ee5
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-19 19:36:38 +0200

    fixed snack 2

--- js snack 2/script.js
+++ js snack 2/script.js
@@ -1,17 +1,26 @@
 
-let numberRandom = Math.random() * 100 + 1;
+let numberRandom = Math.floor(Math.random() * 100 + 1);
+console.log(numberRandom);
 let inputUser = 0;
-let indovinato = true
+let indovinato = false
 
-while(indovinato === false){
 
- inputUser = parseInt(prompt = "Indovina un numero da 1 a 100");
+while(!indovinato){
 
- if(inputUser === numberRandom){
-   console.log("hai indovinato!")
+    inputUser = parseInt(prompt( "Indovina un numero da 1 a 100"));
+    console.log(inputUser);
+
+ if (inputUser < numberRandom){
+    prompt(("il numero è piu grande"));
  }
- else if (inputUser < numberRandom){
-    console.log("il numero ")
+ else if (inputUser > numberRandom){
+    prompt(("il numero è piu piccolo"));
+ }
+ else{
+    indovinato = true;
  }
 
+}
+if(indovinato === true){
+    console.log("hai indovinato!");
 }
