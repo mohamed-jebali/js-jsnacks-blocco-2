33441e104498b0efb52163d3039d92f6cc861d7f
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-22 12:54:37 +0200

    fixed function

--- js snack 4/script.js
+++ js snack 4/script.js
@@ -6,21 +6,20 @@ let firstWord = prompt("inserisci la prima parola");
 let secondWord = prompt("inserisci la seconda parola");
 
 
-console.log(lengthWord(firstWord,secondWord));
-
-function lengthWord (firstWord,secondWord){
-
     if(firstWord.length > secondWord.length){
         console.log("la prima parola è piu lunga");
-        return firstWord
     }
     else if(firstWord.length < secondWord.length){
         console.log("la seconda parola è piu lunga");
-        return secondWord
     }
     else{
         console.log("le due parole hanno la stessa lunghezza " + secondWord,firstWord);
-        return firstWord, secondWord
     }
-}
+
+    function lengthWord (firstWord,secondWord){
+        if(firstWord.length === secondWord.length){
+            return true;
+        }
+        return false;
+    }
 
