5ee7eb9c449af01ee95a8df29a7613f1a4979443
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-22 10:56:17 +0200

    added console.log media

--- js snack 3/script.js
+++ js snack 3/script.js
@@ -1,10 +1,11 @@
 let sum = 0;
 // Calcola la somma e la media dei primi 10 numeri.
 
-for(let i = 0;i <= 10;i++){
+for(let i = 1;i <= 10;i++){
  
     sum += i
 }
 
-console.log(sum);
 let media = sum / 10;
+console.log(sum);
+console.log(media);
