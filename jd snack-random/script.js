02fef27635ec331072123946589ddab01ff84717
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-24 11:42:12 +0200

    added randomGenerator

--- jd snack-random/script.js
+++ jd snack-random/script.js
@@ -1,1 +1,18 @@
 // Creare una funzione che restituisca un array di quattro numeri randomici diversi tra loro tra 1 e 5 inclusi
+
+let listNumber = [];
+ 
+function randomNumber (){
+    
+    randomGenerator = Math.floor(Math.random() * 5) + 1;
+
+    for(i = 0; randomGenerator.length < 4;i++){
+
+        if(randomGenerator){
+            listNumber.push(randomGenerator);
+        }
+    }
+
+    return listNumber;
+}
+console.log(listNumber);
