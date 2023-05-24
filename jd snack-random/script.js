f773f438b0721210cf2f07e65e44ff873a6d9e4a
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-24 11:44:06 +0200

    added let to randomGenerator

--- jd snack-random/script.js
+++ jd snack-random/script.js
@@ -4,7 +4,7 @@ let listNumber = [];
  
 function randomNumber (){
     
-    randomGenerator = Math.floor(Math.random() * 5) + 1;
+    let randomGenerator = Math.floor(Math.random() * 5) + 1;
 
     for(i = 0; randomGenerator.length < 4;i++){
 
