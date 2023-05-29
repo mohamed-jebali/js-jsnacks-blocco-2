924a8f375a9de5dd1c6ff0ee121eef15082ed9cb
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-29 12:13:28 +0200

    added createPeperone

--- js snack peperone/script.js
+++ js snack peperone/script.js
@@ -5,13 +5,26 @@
 
 
 
-let listPeperoni = [];
+let listPeperoni = [
+    createPeperone("Rosso",44,11,66,24),
+    createPeperone("Verde",66,55,77,22),
+    createPeperone("Giallo",77,66,88,22),
+    createPeperone("Nero",77,66,88,22),
+    createPeperone("Bianco",77,66,88,22),
+    createPeperone("Magenta",77,66,88,22),
+    createPeperone("Arancione",77,66,88,22),
+    createPeperone("Grigio",77,66,88,22),
+    createPeperone("Blu",77,66,88,22),
+    createPeperone("Viola",77,66,88,22),
+];
 
 
-function createPeperone (pesoMax,pesoMin,lunghezzaMax,lunghezzaMin){
+function createPeperone (colore,pesoMax,pesoMin,lunghezzaMax,lunghezzaMin){
     let peperone = {
+        colore: colore,
         peso: Math.floor(Math.random() * (pesoMax - pesoMin + 1)) + pesoMin,
         lunghezza: Math.floor(Math.random() * (lunghezzaMax - lunghezzaMin + 1)) + lunghezzaMin
       };
-    
+    return peperone;
 }
+
